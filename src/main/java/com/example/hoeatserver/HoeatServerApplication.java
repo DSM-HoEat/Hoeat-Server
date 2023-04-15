d225891a46a9710532e39002f2cfca7fe14e448b
beargame123 <hoyoung7827@gmail.com>
2023-04-15 21:37:20 +0900

    ⚙️:: project_setting

---
+++ src/main/java/com/example/hoeatserver/HoeatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoeatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoeatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoeatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hoeatserver/HoeatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoeatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoeatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

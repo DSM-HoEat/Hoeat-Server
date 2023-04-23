c1131efa71bfe1705f3883ce877bbc61f7f3c238
beargame123 <hoyoung7827@gmail.com>
2023-04-23 18:15:37 +0900

    ➕:: phone Service add

---
+++ src/main/java/com/example/hoeatserver/infrastructure/phone/service/PhoneService.java
@@ -0,0 +1,73 @@
+package com.example.hoeatserver.infrastructure.phone.service;
+
+import com.example.hoeatserver.infrastructure.phone.dto.request.PhoneRequest;
+import com.example.hoeatserver.infrastructure.phone.entity.PhoneCode;
+import com.example.hoeatserver.infrastructure.phone.repository.CodeRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+
+@Service
+@RequiredArgsConstructor
+public class PhoneService{
+
+    private final CodeRepository codeRepository;
+
+    @Value("${spring.coolsms.devHee.apikey}")
+    private final String apiKey;
+
+
+    @Value("${spring.coolsms.devHee.apisecret}")
+    private final String apiSecret;
+
+    @Value("${spring.coolsms.phone}")
+    private final String myPhone;
+
+    @Value("${spring.coolsms.live.code}")
+    private final Long codeTime;
+
+
+    @Transactional
+    public void execute(PhoneRequest request) {
+
+        Message message = new Message();
+
+        message.setFrom(myPhone);
+        message.setTo(request.getPhoneNumber());
+        message.setText("[호잇] 인증번호 ["+randomCode(request)+"]를 입력해주세요.");
+
+        DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
+
+        SingleMessageSentResponse response = messageService.sendOne(new SingleMessageSendingRequest(message));
+        System.out.println(response);
+    }
+
+    private String randomCode(PhoneRequest request){
+
+        SecureRandom random = new SecureRandom();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) {
+            sb.append(random.nextInt(10));
+        }
+
+        codeRepository.save(
+                new PhoneCode(
+                        request.getPhoneNumber(),
+                        sb.toString(),
+                        codeTime
+                )
+        );
+
+        return sb.toString();
+    }
+}
